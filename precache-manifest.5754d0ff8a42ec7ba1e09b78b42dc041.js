48998ffc8635ec1dafbbb06a7b947514e1d515da
LJFrancis <joe.mengis@gmail.com>
2020-10-02 12:01:32 -0700

    Updates

---
+++ precache-manifest.5754d0ff8a42ec7ba1e09b78b42dc041.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec13cde038849455092f9810707e57c6",
+    "url": "/Mengis_Employee_Directory/index.html"
+  },
+  {
+    "revision": "a7cd199d69ea11de2e80",
+    "url": "/Mengis_Employee_Directory/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "c0bbfb94c32d84d9dc92",
+    "url": "/Mengis_Employee_Directory/static/css/main.6fd2d8c6.chunk.css"
+  },
+  {
+    "revision": "a7cd199d69ea11de2e80",
+    "url": "/Mengis_Employee_Directory/static/js/2.58751126.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/Mengis_Employee_Directory/static/js/2.58751126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0bbfb94c32d84d9dc92",
+    "url": "/Mengis_Employee_Directory/static/js/main.601730f3.chunk.js"
+  },
+  {
+    "revision": "81324ecc39a5e39ec1fa",
+    "url": "/Mengis_Employee_Directory/static/js/runtime-main.21d4bc8e.js"
+  }
+]);
